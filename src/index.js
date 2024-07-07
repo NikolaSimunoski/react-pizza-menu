2659498e034fe370a994c8fb3ac539b9c61977d0
Nikola Simunoski <47476124+NikolaSimunoski@users.noreply.github.com>
2024-07-07 11:04:23 +0200

    -react fragment

--- src/index.js
+++ src/index.js
@@ -71,12 +71,19 @@ function Menu() {
   return (
     <div className="menu">
       <h2>Our menu</h2>
+
       {pizzas.length > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We are still working on our Menu!</p>
       )}
